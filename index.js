ec2076376475cfb9229178a1ac1d15d0804a77a6
gabriel9silv <134931559+gabriel9silv@users.noreply.github.com>
2024-08-16 11:38:15 -0300

    16/08/2024

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
  const express = require('express');
+ const cors = require ('cors');
 
- const app = express();
-
+const router = require('./routes/route');
  //const porta = process.env.PORT || 3333;
- const porta = 3333;
+ const app = express();
+ app.use(cors());
+ app.use(express.json());
+ app.use(router);
+
+ const porta = 3333;0n
 
  app.listen(porta, () => {
     console.log('Servidor iniciado na porta' + porta);
